5ffd8abf763c217ed47111108c9fd04547b42e15
Hcinet Amin <53219971+HcinetA@users.noreply.github.com>
2024-11-27 14:13:51 -0600

    Update prob4.java

--- assignment1/prob4.java
+++ assignment1/prob4.java
@@ -5,28 +5,47 @@ import java.util.Arrays;
 
 
 public class prob4 {
-    public static void main(String[] args) {
-        String[] inputArray = {"horse", "dog", "cat", "horse", "dog"};
+    public static String[] removeDuplicates(String[] inputArray) {
 
-        String[] uniqueArray = removeDuplicates(inputArray);
-        System.out.println(Arrays.toString(uniqueArray));
-    }
+        if (inputArray == null || inputArray.length == 0) {
+            return new String[0];
+        }
 
-    public static String[] removeDuplicates(String[] array) {
-        Arrays.sort(array);
+        String[] tempArray = new String[inputArray.length];
+        int uniqueCount = 0;
 
-        String[] tempArray = new String[array.length];
-        int uniqueIndex = 0;
+        for (String currentString : inputArray) {
+            // Skip null values
+            if (currentString == null) {
+                continue;
+            }
 
-        for (int i = 0; i < array.length; i++) {
-            if (i == 0 || !array[i].equals(array[i - 1])) {
-                tempArray[uniqueIndex++] = array[i];
+            boolean isDuplicate = false;
+            for (int j = 0; j < uniqueCount; j++) {
+                if (currentString.equals(tempArray[j])) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate) {
+                tempArray[uniqueCount] = currentString;
+                uniqueCount++;
             }
         }
 
-        String[] resultArray = new String[uniqueIndex];
-        System.arraycopy(tempArray, 0, resultArray, 0, uniqueIndex);
+        String[] uniqueArray = new String[uniqueCount];
+        System.arraycopy(tempArray, 0, uniqueArray, 0, uniqueCount);
 
-        return resultArray;
+        return uniqueArray;
     }
+
+    public static void main(String[] args) {
+        String[] input = {"horse", "dog", "cat", "horse", "dog"};
+
+        String[] result = removeDuplicates(input);
+
+        System.out.println(Arrays.toString(result));
+    }
+
 }
