1e0cbbe5e13c37c6c745a37368b73787a785c563
Hcinet Amin <53219971+HcinetA@users.noreply.github.com>
2024-12-02 23:09:51 -0600

    Update Exponential.java

--- assignment3/Exponential.java
+++ assignment3/Exponential.java
@@ -1,3 +1,5 @@
+package a3Prob1;
+
 public class Exponential {
 
     public double power(double x, int n) {
@@ -5,18 +7,11 @@ public class Exponential {
             return 1;
         }
 
-        else if (n==1){
-            return x;
-        }
-
-        else if (n < 0 ) {
-            throw new IllegalArgumentException("n can't be negative only use naturel numbers is undefined");
-
+        if (n < 0) {
+            throw new IllegalArgumentException("Negative exponents are not allowed. Please use natural numbers.");
         }
 
         return x * power(x, n - 1);
-
-
     }
 
     public static void main(String[] args) {
